76f4f700b0350a1dc068a49098ea485b94bf8686
Kulsinbai <mcc.kulsinbai@gmail.com>
2025-02-25 00:03:48 +0500

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,18 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +30,17 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        item1.setId(1);
+        item1.setName("Hello");
+        System.out.println("Item 1: id = " + item1.getId() + ", name = " + item1.getName());
+        Item item2 = new Item();
+        item2.setId(2);
+        System.out.println("Item 2: id = " + item2.getId() + ", name = " + item2.getName());
+        Item item3 = new Item(3, "Привет");
+        System.out.println("Item 3: id = " + item3.getId() + ", name = " + item3.getName());
     }
 }
