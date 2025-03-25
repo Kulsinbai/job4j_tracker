8359ab893dc296088f7646faafc1a4a452b351d0
Kulsinbai <mcc.kulsinbai@gmail.com>
2025-03-25 22:55:13 +0500

    5. Tracker - хранилище [#396 #536758]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -33,6 +33,7 @@ public class TrackerTest {
         Item result = tracker.findAll()[0];
         assertThat(result.getName()).isEqualTo(first.getName());
     }
+
     @Test
     public void whenTestFindByNameCheckArrayLength() {
         Tracker tracker = new Tracker();
