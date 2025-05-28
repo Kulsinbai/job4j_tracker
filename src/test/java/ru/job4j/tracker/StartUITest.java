238c6c0b988722498f3a33a65b66550e1a4e0f10
Kulsinbai <mcc.kulsinbai@gmail.com>
2025-05-28 18:23:31 +0500

    1. Обеспечить бесперебойную работу приложения Tracker [#789 #544721]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,16 +10,6 @@ import ru.job4j.tracker.output.StubOutput;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
-    @Test
-    void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new MockInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-    }
 
     @Test
     void whenCreateItem() {
