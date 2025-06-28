6dee4284ed8cecc71081f9046eec18f2fd29947b
Kulsinbai <mcc.kulsinbai@gmail.com>
2025-06-28 10:19:06 +0500

    0. Map, HashMap [#212652 #547003]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> mail = new HashMap<>();
         mail.put("kulsinbai@mail.ru", "Ilyas Kulsinbaev");
+        mail.put("biktairiva@mail.ru", "Elena Biktairova");
+        mail.put("smakov@mail.ru", "Pavel Smakov");
+        mail.put("kulsinbai@mail.ru", "Ilyas Kulsinbai");
 
         for (String key : mail.keySet()) {
             String value = mail.get(key);
