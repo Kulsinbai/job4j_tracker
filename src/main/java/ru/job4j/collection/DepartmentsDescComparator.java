9f25ecaa9615483babfaa8a7de3d3d0346ab6941
kulsinbai <mcc.kulsinbai@gmail.com>
2026-02-26 22:54:49 +0500

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        String leftTop = left.split("/") [0];
+        String rightTop = right.split("/") [0];
+        if (!leftTop.equals(rightTop)) {
+            return rightTop.compareTo(leftTop);
+        }
+        return left.compareTo(right);
+    }
+}
